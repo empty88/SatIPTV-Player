9d4e7f75ba392549321106de2c3371e5480827c2
Marco <mt@mt88.eu>
2025-01-16 22:40:02 +0100

    switched from .NET Framework 4.7.2 to .net 8.0

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -4,18 +4,6 @@ Imports System.Reflection
 Imports System.Resources
 Imports System.Runtime.InteropServices
 Imports System.Windows
-
-' Allgemeine Informationen über eine Assembly werden über die folgenden
-' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-' die einer Assembly zugeordnet sind.
-
-' Werte der Assemblyattribute überprüfen
-
-<Assembly: AssemblyTitle("SatIPTV")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("Marco Thönneßen")>
-<Assembly: AssemblyProduct("Sat>IP TV-Player")>
-<Assembly: AssemblyCopyright("Copyright © Marco Thönneßen 2024")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: ComVisible(false)>
 
@@ -43,17 +31,3 @@ Imports System.Windows
 
 'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
 <Assembly: Guid("762a2d8c-014b-484d-9f2c-8b228abba9f9")>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion
-'      Buildnummer
-'      Revision
-'
-' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-' indem Sie "*" wie unten gezeigt eingeben:
-' <Assembly: AssemblyVersion("1.0.*")>
-
-<Assembly: AssemblyVersion("1.2.0.0")>
-<Assembly: AssemblyFileVersion("1.2.0.0")>
